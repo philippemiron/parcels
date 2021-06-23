2c870138b44702f92e815e61562d99057a1cc4fc
Christian Kehl <c.kehl@uu.nl>
2021-06-23 14:47:33 +0200

    master_benchmark - added benchmarking variants and files

--- parcels/kernel/__init__.py
+++ parcels/kernel/__init__.py
@@ -1,5 +1,6 @@
 from .basekernel import BaseKernel  # noqa
 from .kernelaos import KernelAOS  # noqa
 from .kernelsoa import KernelSOA  # noqa
+from .kernelsoa_benchmark import KernelSOA_Benchmark  # noqa
 
 Kernel = KernelSOA
--- parcels/particleset/__init__.py
+++ parcels/particleset/__init__.py
@@ -1,6 +1,7 @@
 from .baseparticleset import BaseParticleSet  # noqa
 from .particlesetaos import ParticleSetAOS  # noqa
 from .particlesetsoa import ParticleSetSOA  # noqa
+from .particlesetsoa_benchmark import ParticleSetSOA_Benchmark  # noqa
 
 # ParticleSet is an alias for ParticleSetSOA, i.e. the default
 # implementation for storing particles is the Structure of Arrays
